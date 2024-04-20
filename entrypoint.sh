796b8ee66e471f4e902a1d616862a641ee2501b2
Soklaoti <48424042+Soklaoti@users.noreply.github.com>
2024-04-20 07:04:22 +0000

    Create action dockerfile and entrypoint files

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "====================="
+
+#to allow to use git
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${INPUT_EMAIL}"
+
+#to run workflow
+git config --global --add safe.directory /github/workspace
+
+#to run feed.py in docker container, we said python3 because we install python v3
+python3 /usr/bin/feed.py
+
+#push back main branch
+git add -A && git commit -m "Update Feed"
+git push --set-upstream origin main
+
+echo "====================="
